43b5b4631640c9ae61173abe95e1e72672a01c89
leresi <kevinleparwa@gmail.com>
2021-11-03 00:57:47 +0300

    auth

--- src/app/user-make-payment/services/mpesa.service.ts
+++ src/app/user-make-payment/services/mpesa.service.ts
@@ -6,37 +6,18 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MpesaService {
- private username ='Azs2KejU1ARvIL5JdJsARbV2gDrWmpOB'
-private password ='hipGvFJbOxri330c'
   constructor(
     private http:HttpClient
   ) { }
 
-  createAuthorizationHeader(headers: HttpHeaders) {
-    headers.append("Authorization", "Basic " + btoa(this.username+":"+this.password).toString()); 
-  }
-  getToken(){
-let headers = new Headers();
-headers.append("Authorization", "Basic " + btoa(this.username+":"+this.password).toString());
-fetch("/oauth/v1/generate?grant_type=client_credentials",
 
-{headers, mode:'no-cors', })
-  .then(result => console.log(result))
-  .catch(error => console.log(error));
-  // return this.http.get(
-  //   this.url, {headers: new HttpHeaders({
-  //     'username':this.username,
-  //     'password':this.password
-  //   })}
-  // )
-  
-  }
   getDarajaDeveloperToken(){
-    let headers =new HttpHeaders()
-    this.createAuthorizationHeader(headers)
-    return this.http.get<{}>(
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/x-www-urlencoded');
+    headers = headers.append('Authorization', 'Basic ' + btoa('Azs2KejU1ARvIL5JdJsARbV2gDrWmpOB' + ':' + 'hipGvFJbOxri330c'));
+    return this.http.get(
       '/oauth/v1/generate',
-      {headers:headers, withCredentials:true, params:{'grant_type':'client_credentials'}},
+      {headers:headers,  params:{"grant_type":"client_credentials"}},
       )
   }
 }
